9c1fb9ed9a40a792b66c48731d3521940a7db07c
apardyl <apardyl@users.noreply.github.com>
2021-07-14 09:01:50 +0000

    deploy: 9d9723ae9191ef53967c38bd6b03fe09261582c5

---
+++ _nuxt/static/1626253306/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626253306",layout:"default",error:a,state:{ui:{scrolling:false},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fen\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6b7eeab2"}}}}(null));
