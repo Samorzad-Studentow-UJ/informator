8a3f3f0f964e99e033a866a924239b6b0b7885af
apardyl <apardyl@users.noreply.github.com>
2021-07-23 09:37:41 +0000

    deploy: a2d92770af023f98a78f19d3d80ea64beb3c4eab

---
+++ _nuxt/static/1627033058/pl/hello/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/pl/hello", (function(a){return {data:[{article:{slug:"index",title:"Coś",icon:"rocket",order:9999,toc:[],body:{type:"root",children:[]},dir:"\u002Fpl\u002Fhello",path:"\u002Fpl\u002Fhello\u002Findex",extension:".md",createdAt:a,updatedAt:a},_img:{}}],fetch:{},mutations:[]}}("2021-07-23T09:36:06.771Z")));
