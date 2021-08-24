8de81858de2db48a1a8de3e85e469471fd37aca7
apardyl <apardyl@users.noreply.github.com>
2021-08-24 20:22:45 +0000

    deploy: 3b2335a75526b054f388c0f964f6126ee98d8838

---
+++ _nuxt/static/1629836549/pl/docs/admin/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/pl/docs/admin", {data:[{_img:{"/_ipx/images/docs/netlify_ui.png?fit=inside&q=75&s=1200_400":"\u002F_nuxt\u002Fimage\u002F1af25b.png","/_ipx/images/docs/netlify_edit.png?fit=inside&q=75&s=1200_400":"\u002F_nuxt\u002Fimage\u002Fa07660.png"}}],fetch:{},mutations:[]});
